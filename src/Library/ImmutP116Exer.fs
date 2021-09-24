4c61cdb8620ac598b78d744692bed0386d356aed
Edonis Seferi <edonis.seferi@asos.com>
2021-09-24 15:56:21 +0100

    exercise on page 116

---
+++ src/Library/ImmutP116Exer.fs
@@ -0,0 +1,20 @@
+﻿module ImmutP116Exer
+ 
+let clip ceiling (seq: seq<'T>) =
+    seq
+    |> Seq.map (fun n ->
+        match n > ceiling with
+        | true -> ceiling
+        | false -> n)
+
+let clipExample = 
+    [| 1.9; 23.2; 22.; 4.; 19.; |]
+    |> clip 20.
+    |> Seq.iter (fun f -> printf "%f " f)
+
+let extremes s =
+    (Seq.max s, Seq.min s)
+
+let extremesTest = 
+    [| 1.9; 23.2; 22.; 33.2; 4.; 19.; |] 
+    |> extremes
